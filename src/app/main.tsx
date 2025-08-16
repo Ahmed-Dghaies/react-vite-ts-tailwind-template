db0f88156022b028857a21109665eabf16909444
Ahmed Dghaies <dghaies.ahmed7@gmail.com>
2025-08-16 19:17:48 +0200

    fix imports

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-import { store } from "./store/store.ts";
+import { store } from "./store/store";
 
 import App from "./App.tsx";
 
